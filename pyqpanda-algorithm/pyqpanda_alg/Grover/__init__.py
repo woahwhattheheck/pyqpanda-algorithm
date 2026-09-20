08d64c180374bd3ebd2a57b1454a45aea967aa6a
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-20 18:40:38 -0400

    Export BBHT search API

--- pyqpanda-algorithm/pyqpanda_alg/Grover/__init__.py
+++ pyqpanda-algorithm/pyqpanda_alg/Grover/__init__.py
@@ -1,7 +1,18 @@
 '''
-The QFinance module provides tools related to comparator, Quantum amplitude estimation, Grover algorithm, Grover optimization algorithm and QUBO problem solver, which are used to solve problems such as option pricing and portfolio optimization.
+The Grover module provides Grover search, amplitude amplification, adaptive
+search, and BBHT randomized search for an unknown number of marked states.
 '''
 
 from .Grover_core import Grover,amp_operator,GroverAdaptiveSearch,mark_data_reflection,iter_num,iter_analysis
+from .BBHT import BBHTSearch, BBHTResult
 
-__all__ = ["Grover","amp_operator","GroverAdaptiveSearch","mark_data_reflection","iter_num","iter_analysis"]
+__all__ = [
+    "Grover",
+    "amp_operator",
+    "GroverAdaptiveSearch",
+    "mark_data_reflection",
+    "iter_num",
+    "iter_analysis",
+    "BBHTSearch",
+    "BBHTResult",
+]
