4cf6696090c8055b5e9db58caa9c2d79abd37267
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-20 18:48:09 -0400

    feat: export quantum counting API

---
+++ pyqpanda-algorithm/pyqpanda_alg/QuantumCounting/__init__.py
@@ -0,0 +1,7 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+
+"""Quantum-counting public API."""
+
+from .QuantumCounting import QuantumCounting, QuantumCountingResult
+
+__all__ = ["QuantumCounting", "QuantumCountingResult"]
